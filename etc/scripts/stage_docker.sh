41347bbce53ac849afa60170031e62bfa0d5fda5
Chris Done <chrisdone@gmail.com>
2019-11-14 16:02:57 +0100

    Add stack --version

--- etc/scripts/stage_docker.sh
+++ etc/scripts/stage_docker.sh
@@ -3,5 +3,5 @@ set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")/../.."
 mkdir -p etc/docker/_artifacts
 rm -rf .stack-work
-stack build casa-server
+stack --version
 stack install --local-bin-path=etc/docker/_artifacts "$@"
