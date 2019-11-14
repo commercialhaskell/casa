385c13df82fff65e3faaba6b865d72f31f4f0770
Chris Done <chrisdone@gmail.com>
2019-11-14 15:58:24 +0100

    Remove stack work

--- etc/scripts/stage_docker.sh
+++ etc/scripts/stage_docker.sh
@@ -2,6 +2,6 @@
 set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")/../.."
 mkdir -p etc/docker/_artifacts
-stack clean
+rm -rf .stack-work
 stack build casa-server
 stack install --local-bin-path=etc/docker/_artifacts "$@"
