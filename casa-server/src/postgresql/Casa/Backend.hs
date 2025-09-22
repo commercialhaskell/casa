9bd4859d1a74709d242161ba465e38e75359faa9
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2025-09-22 14:25:45 +0100

    Tidy-up formatting

--- casa-server/src/postgresql/Casa/Backend.hs
+++ casa-server/src/postgresql/Casa/Backend.hs
@@ -1,22 +1,28 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+
+{-# LANGUAGE FlexibleContexts          #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE OverloadedStrings         #-}
+{-# LANGUAGE TypeFamilies              #-}
+
 -- |
 
-module Casa.Backend (module X, withBackendPool,manualMigration) where
+module Casa.Backend
+  ( module X
+  , withBackendPool
+  , manualMigration
+  ) where
 
-import Control.Monad.Trans.Reader
-import Data.Functor
-import Database.Persist.Postgresql as X
-import Yesod
+import           Control.Monad.Trans.Reader
+import           Data.Functor
+import           Database.Persist.Postgresql as X
+import           Yesod
 
 withBackendPool = withPostgresqlPool
 
 manualMigration ::
-  (MonadIO m, BackendCompatible SqlBackend backend) =>
-  ReaderT backend m ()
+     (MonadIO m, BackendCompatible SqlBackend backend)
+  => ReaderT backend m ()
 manualMigration =
   void (rawExecute
           "CREATE INDEX IF NOT EXISTS content_created_idx ON content(created);"
--- casa-server/src/sqlite/Casa/Backend.hs
+++ casa-server/src/sqlite/Casa/Backend.hs
@@ -1,10 +1,16 @@
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+
+{-# LANGUAGE GADTs                     #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE TypeFamilies              #-}
+
 -- |
 
-module Casa.Backend (module X, withBackendPool, manualMigration) where
+module Casa.Backend
+  ( module X
+  , withBackendPool
+  , manualMigration
+  ) where
 
 import qualified Data.Text.Encoding as T
 import           Database.Persist.Sqlite as X
