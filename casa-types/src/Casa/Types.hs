1b9894e7ec082564921bc59e107587acce96ddf8
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2025-09-25 07:24:21 +0100

    Add Haddock documentation, explicit imports and exports
    Also some reformatting

--- casa-types/src/Casa/Types.hs
+++ casa-types/src/Casa/Types.hs
@@ -1,43 +1,54 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
--- |
+-- | Types and functions for use with Casa (Content-Addressable Storage
+-- Archive). See <https://casa.stackage.org/>.
 
-module Casa.Types where
+module Casa.Types
+  ( BlobKey (..)
+  , blobKeyHexParser
+  , blobKeyBinaryParser
+  , blobKeyToBuilder
+  ) where
 
-import           Control.Monad
-import           Data.Aeson
+import           Control.Monad ( (>=>) )
+import           Data.Aeson ( FromJSON (..), ToJSON (..), Value (..) )
 import qualified Data.Attoparsec.ByteString as Atto.B
 import qualified Data.Attoparsec.Text as Atto.T
-import           Data.ByteString (ByteString)
+import           Data.ByteString ( ByteString )
 import qualified Data.ByteString.Base16 as Hex
 import qualified Data.ByteString.Builder as S
-import           Data.Hashable
-import           Database.Persist
-import           Database.Persist.Sql
-import           Data.Text (Text)
+import           Data.Hashable ( Hashable )
+import           Data.Text ( Text )
 import qualified Data.Text.Encoding as T
-import           Web.PathPieces
+import           Database.Persist ( PersistField )
+import           Database.Persist.Sql ( PersistFieldSql )
+import           Web.PathPieces ( PathPiece (..) )
 
 -- | A SHA256 key to address blobs.
 newtype BlobKey =
   BlobKey
     { unBlobKey :: ByteString
     }
-  deriving (Read, Eq, Ord, Hashable, PersistField, PersistFieldSql)
+  deriving (Eq, Hashable, PersistField, PersistFieldSql, Ord, Read)
 
 instance Show BlobKey where
+
   show (BlobKey key) = show (Hex.encode key)
 
 instance FromJSON BlobKey where
+
   parseJSON = parseJSON >=> (either fail pure . blobKeyHexParser)
 
 instance ToJSON BlobKey where
+
   toJSON = String . T.decodeUtf8 . Hex.encode . unBlobKey
 
 instance PathPiece BlobKey where
+
   fromPathPiece =
     either (const Nothing) Just .
     blobKeyHexParser
+
   toPathPiece = T.decodeUtf8 . Hex.encode . unBlobKey
 
 -- | Parse a blob key in hex format.
@@ -55,5 +66,6 @@ blobKeyHexParser =
 blobKeyBinaryParser :: Atto.B.Parser BlobKey
 blobKeyBinaryParser = fmap BlobKey (Atto.B.take 32)
 
+-- | Yield a t'S.Builder' value corresponding to the given t'BlobKey' value.
 blobKeyToBuilder :: BlobKey -> S.Builder
 blobKeyToBuilder = S.byteString . unBlobKey
